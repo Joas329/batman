7b2616daf614daafceaae2e016f64d5b50a1de2f
Joas329 <joaquinphilco@hotmail.com>
2023-11-01 21:09:07 -0400

    treewide: implement batman mce stub
    having a custom libmce glib object with batman wrappers will allow ofono to set the correct states depending on reports from batman wrappers which is required when using phosh

---
+++ src/mce_charger.c
@@ -0,0 +1,341 @@
+/*
+ * Copyright (C) 2019-2022 Jolla Ltd.
+ * Copyright (C) 2019-2022 Slava Monich <slava.monich@jolla.com>
+ *
+ * You may use this file under the terms of BSD license as follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
+ */
+
+#include "mce_charger.h"
+#include "mce_proxy.h"
+#include "mce_log_p.h"
+#include <string.h>
+#include <gutil_misc.h>
+#include "batman-wrappers.h"
+
+#define MCE_CHARGER_STATE_ON        "on"
+#define MCE_CHARGER_STATE_OFF       "off"
+#define MCE_CHARGER_STATE_UNKNOWN   "unknown"
+
+/* Generated headers */
+// #include "org.ayatana.indicator.power.Battery.h"
+
+struct mce_charger_priv {
+    MceProxy* proxy;
+    gulong proxy_valid_id;
+    gulong charger_state_ind_id;
+};
+
+enum mce_charger_signal {
+    SIGNAL_VALID_CHANGED,
+    SIGNAL_STATE_CHANGED,
+    SIGNAL_COUNT
+};
+
+#define SIGNAL_VALID_CHANGED_NAME   "mce-charger-valid-changed"
+#define SIGNAL_STATE_CHANGED_NAME   "mce-charger-state-changed"
+
+static guint mce_charger_signals[SIGNAL_COUNT] = { 0 };
+
+typedef GObjectClass MceChargerClass;
+G_DEFINE_TYPE(MceCharger, mce_charger, G_TYPE_OBJECT)
+#define PARENT_CLASS mce_charger_parent_class
+#define MCE_CHARGER_TYPE (mce_charger_get_type())
+#define MCE_CHARGER(obj) (G_TYPE_CHECK_INSTANCE_CAST(obj,\
+        MCE_CHARGER_TYPE,MceCharger))
+
+/*==========================================================================*
+ * Implementation
+ *==========================================================================*/
+
+// static
+// void
+// mce_charger_state_update(
+//     MceCharger* self,
+//     const char* value)
+// {
+//     MCE_CHARGER_STATE state;
+//     MceChargerPriv* priv = self->priv;
+
+//     if (!g_strcmp0(value, MCE_CHARGER_STATE_ON)) {
+//         state = MCE_CHARGER_ON;
+//     } else if (!g_strcmp0(value, MCE_CHARGER_STATE_OFF)) {
+//         state = MCE_CHARGER_OFF;
+//     } else {
+//         GASSERT(!g_strcmp0(value, MCE_CHARGER_STATE_UNKNOWN));
+//         state = MCE_CHARGER_UNKNOWN;
+//     }
+//     if (self->state != state) {
+//         self->state = state;
+//         g_signal_emit(self, mce_charger_signals[SIGNAL_STATE_CHANGED], 0);
+//     }
+//     if (priv->proxy->valid && !self->valid) {
+//         self->valid = TRUE;
+//         g_signal_emit(self, mce_charger_signals[SIGNAL_VALID_CHANGED], 0);
+//     }
+// }
+
+// static void
+// on_properties_changed (GDBusProxy          *proxy,
+//                        GVariant            *changed_properties,
+//                        const gchar* const  *invalidated_properties,
+//                        gpointer             user_data)
+// {
+//   GVariantIter iter;
+//   const gchar *key;
+//   GVariant *value;
+
+//   g_variant_iter_init (&iter, changed_properties);
+//   while (g_variant_iter_next (&iter, "{&sv}", &key, &value))
+//     {
+//         if (!g_strcmp0(key, "IsDischarging")) {
+//             gboolean state;
+//             g_variant_get (value, "b", &state);
+//             mce_charger_state_update(MCE_CHARGER(user_data), state ? MCE_CHARGER_STATE_OFF : MCE_CHARGER_STATE_ON);
+//         }
+
+//       g_variant_unref (value);
+//     }
+// }
+
+static
+void
+mce_charger_state_query(
+    MceCharger* self)
+{
+    // MceChargerPriv* priv = self->priv;
+    // MceProxy* proxy = priv->proxy;
+
+    UpClient *upower = up_client_new();
+    gdouble battery_percentage;
+
+    const gchar *statelabel = findBattery(upower, &battery_percentage);
+    printf("Battery found");
+
+
+    if(!strcmp((const char*)statelabel,"discharging"))
+    {
+        self->valid = 1;
+        self->state = MCE_CHARGER_ON;
+        printf("Battery State: Discharging");
+    }
+    else if (!strcmp((const char*)statelabel,"charged"))
+    {
+        self->valid = 0;
+        self ->state = MCE_CHARGER_OFF;
+        printf("Battery State: charged");
+    }
+    else if (!strcmp((const char*)statelabel,"fully charged"))
+    {
+        self->valid = 0;
+        printf("Battery State: Charged");
+    }
+
+    else 
+    {
+        self->state = MCE_CHARGER_UNKNOWN;
+    }
+
+    /*
+     * proxy->signal and proxy->request may not be available at the
+     * time when MceCharger is created. In that case we have to wait
+     * for the valid signal before we can connect the charger state
+     * signal and submit the initial query.
+     */
+    // if (proxy->ayatana_request && !priv->charger_state_ind_id) {
+    //     priv->charger_state_ind_id = g_signal_connect(proxy->ayatana_request, "g-properties-changed",
+    //             G_CALLBACK(on_properties_changed), self);
+    // }
+
+    // if (proxy->ayatana_request && proxy->valid) {
+    //     gboolean state = org_ayatana_indicator_power_battery_get_is_discharging(proxy->ayatana_request);
+    //     mce_charger_state_update(self, state ? MCE_CHARGER_STATE_OFF : MCE_CHARGER_STATE_ON);
+    // }
+}
+
+// static
+// void
+// mce_charger_valid_changed(
+//     MceProxy* proxy,
+//     void* arg)
+// {
+//     MceCharger* self = MCE_CHARGER(arg);
+
+//     if (proxy->valid) {
+//         mce_charger_state_query(self);
+//     } else {
+//         if (self->valid) {
+//             self->valid = FALSE;
+//             g_signal_emit(self, mce_charger_signals[SIGNAL_VALID_CHANGED], 0);
+//         }
+//     }
+// }
+// MCE_CHARGER_ON
+/*==========================================================================*
+ * API
+ *==========================================================================*/
+
+MceCharger*
+mce_charger_new()
+{
+    /* MCE assumes one charger */
+    static MceCharger* mce_charger_instance = NULL;
+
+    if (mce_charger_instance) {
+        mce_charger_ref(mce_charger_instance);
+    } else {
+        mce_charger_instance = g_object_new(MCE_CHARGER_TYPE, NULL);
+        mce_charger_state_query(mce_charger_instance);
+        g_object_add_weak_pointer(G_OBJECT(mce_charger_instance),
+            (gpointer*)(&mce_charger_instance));
+    }
+    return mce_charger_instance;
+}
+
+MceCharger*
+mce_charger_ref(
+    MceCharger* self)
+{
+    if (G_LIKELY(self)) {
+        g_object_ref(MCE_CHARGER(self));
+    }
+    return self;
+}
+
+void
+mce_charger_unref(
+    MceCharger* self)
+{
+    if (G_LIKELY(self)) {
+        g_object_unref(MCE_CHARGER(self));
+    }
+}
+
+gulong
+mce_charger_add_valid_changed_handler(
+    MceCharger* self,
+    MceChargerFunc fn,
+    void* arg)
+{
+    return (G_LIKELY(self) && G_LIKELY(fn)) ? g_signal_connect(self,
+        SIGNAL_VALID_CHANGED_NAME, G_CALLBACK(fn), arg) : 0;
+}
+
+gulong
+mce_charger_add_state_changed_handler(
+    MceCharger* self,
+    MceChargerFunc fn,
+    void* arg)
+{
+    return (G_LIKELY(self) && G_LIKELY(fn)) ? g_signal_connect(self,
+        SIGNAL_STATE_CHANGED_NAME, G_CALLBACK(fn), arg) : 0;
+}
+
+void
+mce_charger_remove_handler(
+    MceCharger* self,
+    gulong id)
+{
+    if (G_LIKELY(self) && G_LIKELY(id)) {
+        g_signal_handler_disconnect(self, id);
+    }
+}
+
+void
+mce_charger_remove_handlers(
+    MceCharger* self,
+    gulong* ids,
+    guint count)
+{
+    gutil_disconnect_handlers(self, ids, count);
+}
+
+/*==========================================================================*
+ * Internals
+ *==========================================================================*/
+
+static
+void
+mce_charger_init(
+    MceCharger* self)
+{
+    MceChargerPriv* priv = G_TYPE_INSTANCE_GET_PRIVATE(self, MCE_CHARGER_TYPE,
+        MceChargerPriv);
+
+    // self->priv = priv;
+    // priv->proxy = mce_proxy_new();
+    // priv->proxy_valid_id = mce_proxy_add_valid_changed_handler(priv->proxy,
+    //     mce_charger_valid_changed, self);
+}
+
+static
+void
+mce_charger_finalize(
+    GObject* object)
+{
+    MceCharger* self = MCE_CHARGER(object);
+    MceChargerPriv* priv = self->priv;
+
+    // if (priv->charger_state_ind_id) {
+    //     g_signal_handler_disconnect(priv->proxy->ayatana_request,
+    //         priv->charger_state_ind_id);
+    // }
+    // mce_proxy_remove_handler(priv->proxy, priv->proxy_valid_id);
+    // mce_proxy_unref(priv->proxy);
+    G_OBJECT_CLASS(PARENT_CLASS)->finalize(object);
+}
+
+static
+void
+mce_charger_class_init(
+    MceChargerClass* klass)
+{
+    GObjectClass* object_class = G_OBJECT_CLASS(klass);
+
+    object_class->finalize = mce_charger_finalize;
+    // g_type_class_add_private(klass, sizeof(MceChargerPriv));
+    mce_charger_signals[SIGNAL_VALID_CHANGED] =
+        g_signal_new(SIGNAL_VALID_CHANGED_NAME,
+            G_OBJECT_CLASS_TYPE(klass), G_SIGNAL_RUN_FIRST,
+            0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+    mce_charger_signals[SIGNAL_STATE_CHANGED] =
+        g_signal_new(SIGNAL_STATE_CHANGED_NAME,
+            G_OBJECT_CLASS_TYPE(klass), G_SIGNAL_RUN_FIRST,
+            0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+}
+
+/*
+ * Local Variables:
+ * mode: C
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ src/mce_proxy.c
@@ -0,0 +1,318 @@
+/*
+ * Copyright (C) 2016 Jolla Ltd.
+ * Contact: Slava Monich <slava.monich@jolla.com>
+ *  * Copyright (C) 2023 Droidian Project
+ * Copyright (C) 2023 Joaquin Philco <joaquinphilco@gmail.coom>
+ *
+ * You may use this file under the terms of BSD license as follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of Jolla Ltd nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
+ */
+
+#include "mce_proxy.h"
+#include "mce_log_p.h"
+
+#define MCE_SERVICE "org.droidian.batman.wlrdisplay'
+#define MCE_REQUEST_PATH "/com/canonical/Unity/Screen"
+#define MCE_SIGNAL_PATH "/com/canonical/Unity/Screen"
+
+struct _OrgDroidianBatmanWlrdisplay;
+typdef struct _OrgDroidianBatmanWlrdisplay OrgDroidianBatmanWlrDisplay;
+
+GLOG_MODULE_DEFINE("mce");
+
+struct mce_proxy_priv {
+    GDBusConnection* bus;
+    guint mce_watch_id;
+};
+
+enum mce_proxy_signal {
+    SIGNAL_VALID_CHANGED,
+    SIGNAL_COUNT
+};
+
+#define SIGNAL_VALID_CHANGED_NAME   "mce-proxy-valid-changed"
+
+static guint mce_proxy_signals[SIGNAL_COUNT] = { 0 };
+
+typedef GObjectClass MceProxyClass;
+G_DEFINE_TYPE(MceProxy, mce_proxy, G_TYPE_OBJECT)
+#define PARENT_CLASS mce_proxy_parent_class
+#define MCE_PROXY_TYPE (mce_proxy_get_type())
+#define MCE_PROXY(obj) (G_TYPE_CHECK_INSTANCE_CAST(obj,\
+        MCE_PROXY_TYPE,MceProxy))
+
+static
+void
+mce_name_appeared(
+    GDBusConnection* bus,
+    const gchar* name,
+    const gchar* owner,
+    gpointer arg)
+{
+    MceProxy* self = MCE_PROXY(arg);
+
+    GDEBUG("Name '%s' is owned by %s", name, owner);
+    GASSERT(!self->valid);
+    self->valid = TRUE;
+    g_signal_emit(self, mce_proxy_signals[SIGNAL_VALID_CHANGED], 0);
+}
+
+static
+void
+mce_name_vanished(
+    GDBusConnection* bus,
+    const gchar* name,
+    gpointer arg)
+{
+    MceProxy* self = MCE_PROXY(arg);
+
+    GDEBUG("Name '%s' has disappeared", name);
+    if (self->valid) {
+        self->valid = FALSE;
+        g_signal_emit(self, mce_proxy_signals[SIGNAL_VALID_CHANGED], 0);
+    }
+}
+
+static
+void
+mce_proxy_init_check(
+    MceProxy* self)
+{
+    MceProxyPriv* priv = self->priv;
+
+    if (self->signal && self->request && !priv->mce_watch_id) {
+        priv->mce_watch_id = g_bus_watch_name_on_connection(priv->bus,
+            NULL, G_BUS_NAME_WATCHER_FLAGS_NONE,
+            mce_name_appeared, mce_name_vanished, self, NULL);
+    }
+}
+
+static
+void
+mce_proxy_request_proxy_new_finished(
+    GObject* object,
+    GAsyncResult* result,
+    gpointer arg)
+{
+    MceProxy* self = MCE_PROXY(arg);
+    GError* error = NULL;
+
+    GASSERT(!self->request);
+    self->request = com_canonical_unity_screen_proxy_new_finish(result, &error);
+    if (self->request) {
+        mce_proxy_init_check(self);
+    } else {
+        GERR("Failed to initialize MCE request proxy: %s", GERRMSG(error));
+        g_error_free(error);
+    }
+    mce_proxy_unref(self);
+}
+
+OrgDroidianBatmanWlrdisplay *
+org_droidian_batman_wlrdisplay_new_finish (
+    GAsynch         *res,
+    GError          **error)
+{
+    GObject *ret;
+    GObject *source_object;
+    source_object = g_async_result_get_source_object (res);
+    ret = g_async_initable_new_finish (G_ASYNC_INITABLE (source_object), res, error);
+    g_object_unref (source_object);
+    if (ret != NULL)
+        return COM_CANONICAL_UNITY_SCREEN (ret);
+    else
+        return NULL;
+}
+
+
+static
+void
+mce_proxy_signal_proxy_new_finished(
+    GObject* object,
+    GAsyncResult* result,
+    gpointer arg)
+{
+    MceProxy* self = MCE_PROXY(arg);
+    GError* error = NULL;
+
+    GASSERT(!self->signal);
+    self->signal = com_canonical_unity_screen_proxy_new_finish(result, &error);
+    if (self->signal) {
+        mce_proxy_init_check(self);
+    } else {
+        GERR("Failed to initialize MCE signal proxy: %s", GERRMSG(error));
+        g_error_free(error);
+    }
+    mce_proxy_unref(self);
+}
+
+static
+void
+mce_proxy_bus_get_finished(
+    GObject* object,
+    GAsyncResult* result,
+    gpointer arg)
+{
+    MceProxy* self = MCE_PROXY(arg);
+    MceProxyPriv* priv = self->priv;
+    GError* error = NULL;
+
+    priv->bus = g_bus_get_finish(result, &error);
+    if (priv->bus) {
+        g_asynch_initiable_new_asynch(G_GNUC_CONST,G_PRIORITY_DEFAULT, NULL, mce_proxy_request_proxy_new_finished, 
+        mce_proxy_ref(self), "g-flags", G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES, 
+        "g-name", MCE_SERVICE, "g-connection", priv->bus,
+        "g-object-path", MCE_REQUEST_PATH, "g-interface-name", "org.droidian.batman.wlrdisplay", NULL);
+
+        g_asynch_initiable_new_asynch(G_GNUC_CONST,G_PRIORITY_DEFAULT, NULL, mce_proxy_request_proxy_new_finished, 
+        mce_proxy_ref(self), "g-flags", G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES, 
+        "g-name", MCE_SERVICE, "g-connection", priv->bus,
+        "g-object-path", MCE_SIGNAL_PATH, "g-interface-name", "org.droidian.batman.wlrdisplay", NULL);
+        
+    } else {
+        GERR("Failed to attach to system bus: %s", GERRMSG(error));
+        g_error_free(error);
+    }
+    mce_proxy_unref(self);
+}
+
+MceProxy*
+mce_proxy_new()
+{
+    /*
+     * Since there's only one mce in the system, there's no need for
+     * more than one proxy object.
+     */
+    static MceProxy* mce_proxy_instance = NULL;
+    if (mce_proxy_instance) {
+        mce_proxy_ref(mce_proxy_instance);
+    } else {
+        mce_proxy_instance = g_object_new(MCE_PROXY_TYPE, NULL);
+        g_bus_get(G_BUS_TYPE_SYSTEM, NULL, mce_proxy_bus_get_finished,
+            mce_proxy_ref(mce_proxy_instance));
+        g_object_add_weak_pointer(G_OBJECT(mce_proxy_instance),
+            (gpointer*)(&mce_proxy_instance));
+    }
+    return mce_proxy_instance;
+}
+
+MceProxy*
+mce_proxy_ref(
+    MceProxy* self)
+{
+    if (G_LIKELY(self)) {
+        g_object_ref(MCE_PROXY(self));
+    }
+    return self;
+}
+
+void
+mce_proxy_unref(
+    MceProxy* self)
+{
+    if (G_LIKELY(self)) {
+        g_object_unref(MCE_PROXY(self));
+    }
+}
+
+gulong
+mce_proxy_add_valid_changed_handler(
+    MceProxy* self,
+    MceProxyFunc fn,
+    void* arg)
+{
+    return (G_LIKELY(self) && G_LIKELY(fn)) ? g_signal_connect(self,
+        SIGNAL_VALID_CHANGED_NAME, G_CALLBACK(fn), arg) : 0;
+}
+
+void
+mce_proxy_remove_handler(
+    MceProxy* self,
+    gulong id)
+{
+    if (G_LIKELY(self) && G_LIKELY(id)) {
+        g_signal_handler_disconnect(self, id);
+    }
+}
+
+static
+void
+mce_proxy_init(
+    MceProxy* self)
+{
+    self->priv = G_TYPE_INSTANCE_GET_PRIVATE(self,
+        MCE_PROXY_TYPE, MceProxyPriv);
+}
+
+static
+void
+mce_proxy_finalize(
+    GObject* object)
+{
+    MceProxy* self = MCE_PROXY(object);
+    MceProxyPriv* priv = self->priv;
+
+    if (priv->mce_watch_id) {
+        g_bus_unwatch_name(priv->mce_watch_id);
+    }
+    if (self->signal) {
+        g_object_unref(self->signal);
+    }
+    if (self->request) {
+        g_object_unref(self->request);
+    }
+    if (priv->bus) {
+        g_object_unref(priv->bus);
+    }
+    G_OBJECT_CLASS(PARENT_CLASS)->finalize(object);
+}
+
+static
+void
+mce_proxy_class_init(
+    MceProxyClass* klass)
+{
+    GObjectClass* object_class = G_OBJECT_CLASS(klass);
+    object_class->finalize = mce_proxy_finalize;
+    g_type_class_add_private(klass, sizeof(MceProxyPriv));
+    mce_proxy_signals[SIGNAL_VALID_CHANGED] =
+        g_signal_new(SIGNAL_VALID_CHANGED_NAME,
+            G_OBJECT_CLASS_TYPE(klass), G_SIGNAL_RUN_FIRST,
+            0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+}
+
+/*
+ * Local Variables:
+ * mode: C
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
